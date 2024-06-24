c1668c91f834898a28f3f39b9a0f94e93567b044
ahakiru <your.maoka@googlemail.com>
2024-06-24 07:19:54 +0200

    Add hovering function.

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let body = document.querySelector("body");
-let squares = [];
+
 for (i=0; i<16; i++) {
     for (j=0; j<16; j++) {
         const currentSquare = document.createElement("div");
-        /*currentSquare.innerHTML = `square${i}+${j}`;*/
+        currentSquare.id = `${i}${j}`;
         body.appendChild(currentSquare);
         };
     };
-
+let div = document.querySelectorAll("div");
+div.forEach(function (element){
+    element.addEventListener("mouseenter", (event) => {
+        element.id = "hovered";
+    })
+}) 

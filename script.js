6f35d4802b7be32f7ca8c9cd9f2cd7238d58cab1
ahakiru <your.maoka@googlemail.com>
2024-06-26 16:10:21 +0200

    Add create/clear button

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-let body = document.querySelector("body");
+const container = document.querySelector(".container");
 
-for (i=0; i<16; i++) {
-    for (j=0; j<16; j++) {
-        const currentSquare = document.createElement("div");
-        currentSquare.id = `${i}${j}`;
-        body.appendChild(currentSquare);
-        };
+let btn = document.querySelector("button");
+
+function clearGrid() {
+    container.innerHTML = '';
+};
+let div = container.querySelectorAll("div");
+btn.addEventListener("click", function() {
+    clearGrid();
+    let promptString = prompt("How many squares should be in each row of the grid?", "");
+    let userEntry = parseInt(promptString);
+    if(userEntry > 100) {
+        userEntry = 100;
     };
-let div = document.querySelectorAll("div");
-div.forEach(function (element){
-    element.addEventListener("mouseenter", (event) => {
-        element.id = "hovered";
-    })
-}) 
+    for (i=0; i<userEntry; i++) {
+        for (j=0; j<userEntry; j++) {
+            const currentSquare = document.createElement("div");
+            currentSquare.id = `${i}${j}`;
+            container.appendChild(currentSquare);
+            };
+        };
+    div = container.querySelectorAll("div");
+    let squareSize = 400/userEntry;
+    squareSize = squareSize+'px';
+    div.forEach(function (element){
+        element.style.height = squareSize;
+        element.style.width = squareSize;
+     });
+    div.forEach(function (square) {
+       square.addEventListener("mouseenter", (event) => {
+           square.id = "hovered";
+        });
+    });
+});
+
+container.addEventListener("click", (event) => {
+    console.log("click");
+});

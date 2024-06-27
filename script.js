65ace93605528c984932e37d21727ef105758357
ahakiru <your.maoka@googlemail.com>
2024-06-27 16:35:11 +0200

    Add additional buttons, put create in own function and add random colors.

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const container = document.querySelector(".container");
 
-let btn = document.querySelector("button");
+let create = document.querySelector("#create");
+let black = document.querySelector("#black");
+let random = document.querySelector("#random");
 
 function clearGrid() {
     container.innerHTML = '';
 };
-let div = container.querySelectorAll("div");
-btn.addEventListener("click", function() {
-    clearGrid();
+function createGrid() {
     let promptString = prompt("How many squares should be in each row of the grid?", "");
     let userEntry = parseInt(promptString);
     if(userEntry > 100) {
         userEntry = 100;
+        alert("The grid is generated to a maximum of 100 squares per row.")
     };
     for (i=0; i<userEntry; i++) {
         for (j=0; j<userEntry; j++) {
             const currentSquare = document.createElement("div");
-            currentSquare.id = `${i}${j}`;
             container.appendChild(currentSquare);
             };
         };
@@ -27,12 +27,39 @@ btn.addEventListener("click", function() {
         element.style.height = squareSize;
         element.style.width = squareSize;
      });
+};
+function randomColor() {
+    let value = Math.floor(Math.random() * 255);
+    return value;
+};
+
+let div = container.querySelectorAll("div");
+
+create.addEventListener("click", function () {
+    clearGrid(); /* removes grid, doesn't create a new one yet*/
+});
+
+black.addEventListener("click", function() {
+    clearGrid();
+    createGrid();
     div.forEach(function (square) {
        square.addEventListener("mouseenter", (event) => {
-           square.id = "hovered";
+           square.id = "blackened";
         });
     });
 });
+random.addEventListener("click", function() {
+    clearGrid();
+    createGrid();
+    div.forEach(function (square) {
+        square.addEventListener("mouseenter", (event) => {
+            let value1 = randomColor();
+            let value2 = randomColor();
+            let value3 = randomColor();
+            square.style.backgroundColor = `rgb(${value1}, ${value2}, ${value3})`;
+         });
+     });
+})
 
 container.addEventListener("click", (event) => {
     console.log("click");
